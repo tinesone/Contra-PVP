34b445b4471a9d35abd55caad60f248dda796f3f
Unknown <jetse@verschurendegroot.nl>
2017-11-11 21:30:01 +0100

    Began working on bullets

---
+++ Assets/scripts/BulletController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+
+	public GameObject owner;
+	public int direction = 2;
+	public int speed = 1;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		switch(direction){
+			case 0:
+				break;
+			case 1:
+				break;
+			case 2:
+				transform.position += Vector3.right * speed * Time.deltaTime;
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+			case 7:
+					break;
+		}
+	}
+
+	void OnBecameInvisible () {
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float velocity = 6.5f;
+	public float jump = 3.0f;
+	public GameObject bulletPrefab;
+	public Vector2 gunOffset;
+
+	// Use this for initialization
+	void Start () {}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.A)) {
+			transform.rotation = Quaternion.Euler(0, -180, 0);
+			transform.position += Vector3.left * velocity * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.rotation = Quaternion.Euler (0, 0, 0);
+			transform.position += Vector3.right * velocity * Time.deltaTime;
+		}
+		if(Input.GetKeyDown("space")){
+			shoot();
+			print ("piew piew");
+			print(transform.position.x);
+			print(transform.position.x + gunOffset.x);
+		}
+	}
+
+	void shoot(){
+		Instantiate(bulletPrefab, new Vector3(transform.position.x + gunOffset.x, transform.position.y + gunOffset.y, 0), this.transform.rotation );
+	}
+}
