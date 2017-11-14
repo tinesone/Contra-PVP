8a26f03eac75872917ddaa50823139889b3012b3
tinesone <tlr3@live.nl>
2017-11-14 15:04:16 +0100

    Added jump
    Stats have to be tweaked

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public float velocity = 6.5f;
-	public float jump = 3.0f;
 	public GameObject bulletPrefab;
 	public Vector2 gunOffset;
+
+	public int jump = 6;
+	bool jumpEnable = false;
+
+	public Rigidbody2D rigid;
 
 	// Use this for initialization
 	void Start () {}
@@ -25,10 +29,18 @@ public class PlayerController : MonoBehaviour {
 		if(Input.GetKeyDown("space")){
 			shoot();
 		}
+		if (Input.GetKey (KeyCode.W) && jumpEnable == true) {
+			rigid.AddForce (Vector3.up * (jump * 10));
+			jumpEnable = false;
+		}
+			
 	}
 
 	void shoot(){
 		GameObject bullet = (GameObject)Instantiate(bulletPrefab, new Vector3(transform.position.x + gunOffset.x, transform.position.y + gunOffset.y, 0), Quaternion.identity );
 		bullet.GetComponent<BulletController>().direction = 2;
+	}
+	void OnCollisionEnter2D(Collision2D col){
+		jumpEnable = true;
 	}
 }
